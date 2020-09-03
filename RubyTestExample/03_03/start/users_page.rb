abd1a213be3d5962965f2d751dbfa67fe03a88e0
Eduardo Urzua <EduardoUrzuaBo@gmail.com>
2020-09-03 00:00:55 -0400

    Selenium Test Examples
    In Ruby and Java

---
+++ RubyTestExample/03_03/start/users_page.rb
@@ -0,0 +1,20 @@
+class UsersPage
+
+#css selector 
+SUCCESS_BANNER = { id: 'flash_success'}
+
+attr_reader :driver
+
+	#class methods 
+def initialize (driver)
+	@driver = driver 
+end
+		
+def get_banner_text()
+	banner = @driver.find_element(SUCCESS_BANNER)
+	banner.text 
+end
+
+
+end
+
